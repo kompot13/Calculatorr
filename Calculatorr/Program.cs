5c79c4717f05b49920306b4e5ad98563528445a0
Kate <Kate@HOME-PC>
2022-01-24 13:27:00 +0300

    Add project files.

---
+++ Calculatorr/Program.cs
@@ -0,0 +1,52 @@
+﻿while (true)
+{
+    Console.Clear();
+    double firstValue, secondValue;
+    string action;
+
+    try
+    {
+        Console.WriteLine("Введите первое число");
+        firstValue = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число");
+        secondValue = double.Parse(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Ошибка! Попробуйте заново");
+        Console.ReadLine();
+        continue;
+    }
+
+    Console.WriteLine("Выберите доступную операцию: сложение, вычитание, умножение, деление");
+    action = Console.ReadLine();
+
+    switch (action)
+    {
+        case "+":
+            Console.WriteLine(firstValue + secondValue);
+            break;
+
+        case "-":
+            Console.WriteLine(firstValue - secondValue);
+            break;
+
+        case "*":
+            Console.WriteLine(firstValue * secondValue);
+            break;
+
+        case "/":
+            if (secondValue == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(firstValue / secondValue);
+            }
+            break;
+    }
+
+    Console.ReadLine();
+}
